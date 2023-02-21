55ebedc98a6386aa44423dd34d030b7cd26da640
Erik Hinkelmanns <erik.hinkelmanns@stud.hs-hannover.de>
2023-02-21 02:03:33 +0100

    - added update and delete methods for Category

--- src/controllers/CategoryController.ts
+++ src/controllers/CategoryController.ts
@@ -8,10 +8,11 @@ const getCategory = async (req: Request, res: Response, next: NextFunction) => {
         message: response
     });
 };
+
 const createCategory = async (req: Request, res: Response, next: NextFunction) => {
     let name: string = req.body.name;
-    let budget = req.body.budget;
-    try{
+    let budget: number = req.body.budget;
+    try {
         let response = await connection.category.create({
                 data: {
                     name: name,
@@ -23,13 +24,53 @@ const createCategory = async (req: Request, res: Response, next: NextFunction) =
         return res.status(200).json({
             message: response
         });
-    } catch (handleRequestError){
-        return res.status(400).json({
-            message: "Category Already exists"
+    } catch (handleRequestError) {
+        return res.status(409).json({
+            message: "Category already exists"
         });
     }
-
-
 }
 
-export default {getCategory,createCategory};
+const updateCategory = async (req: Request, res: Response, next: NextFunction) => {
+    let id: string = req.params.id;
+    let name: string = req.body.name;
+    let budget: number = req.body.budget;
+    try{
+        let response = await connection.category.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                name: name,
+                budget: budget
+            }
+        })
+        return res.status(200).json({
+            message: response
+        });
+    } catch (handleRequestError) {
+        return res.status(404).json({
+            message: "Category does not exists"
+        });
+    }
+}
+
+const deleteCategory = async (req: Request, res: Response, next: NextFunction) => {
+    let id: string = req.params.id;
+    try {
+        let response = await connection.category.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        return res.status(200).json({
+            message: "Category deleted successfully"
+        });
+    } catch (e){
+        return res.status(404).json({
+            message: "Category does not exists"
+        });
+    }
+}
+
+export default {getCategory, createCategory, updateCategory, deleteCategory};
--- src/routes/CategoryRoute.ts
+++ src/routes/CategoryRoute.ts
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.get('/category/:name',categoryController.getCategory)
 router.post('/category',categoryController.createCategory)
+router.put('/category/:id',categoryController.updateCategory)
+router.delete('/category/:id',categoryController.deleteCategory)
+
 
 export = router;
