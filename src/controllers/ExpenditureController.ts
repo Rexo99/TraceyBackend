0e0373c7e0515b0919ebc2f5b592b7a68e5df692
Dannie Krösche <dannie.kroesche@stud.hs-hannover.de>
2023-07-24 21:44:20 +0200

    added getImage, removed imageId from Image

--- src/controllers/ExpenditureController.ts
+++ src/controllers/ExpenditureController.ts
@@ -159,11 +159,30 @@ const testUpload = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
+const getImageById = async (req: Request, res: Response, next: NextFunction) => {
+    let id: string = req.params.id
+    try {
+        let response = await connection.image.findUniqueOrThrow({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        return res.status(200).json({
+            message: response
+        });
+    } catch (handleRequestError) {
+        return res.status(404).json({
+            message: "Expenditure does not exists"
+        });
+    }
+}
+
 export default {
     getExpenditure,
     createExpenditure,
     updateExpenditure,
     deleteExpenditure,
     getAllExpendituresByUser,
-    testUpload
+    testUpload,
+    getImageById
 };
